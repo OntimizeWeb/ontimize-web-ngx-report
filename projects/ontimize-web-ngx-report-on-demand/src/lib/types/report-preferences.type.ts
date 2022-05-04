0070c1c4c73058b9e67aba0ef6d89a57cdb3d6de
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2022-05-04 10:23:40 +0200

    Added Types

---
+++ projects/ontimize-web-ngx-report-on-demand/src/lib/types/report-preferences.type.ts
@@ -0,0 +1,9 @@
+export type OReportPreferences = {
+  columns: Array<string>,
+  functions: Array<string>,
+  groups: Array<string>,
+  title: string
+  subtitle: string,
+  styleFunctions: Array<string>,
+  vertical: number
+}
