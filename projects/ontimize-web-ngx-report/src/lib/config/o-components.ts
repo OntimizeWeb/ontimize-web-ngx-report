0af74afed1e6e22a6fe170c927f9353d3db4562a
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2022-06-28 13:41:24 +0200

    Removed exports option in OReportModule

--- projects/ontimize-web-ngx-report/src/lib/config/o-components.ts
+++ projects/ontimize-web-ngx-report/src/lib/config/o-components.ts
@@ -3,7 +3,6 @@ import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
-import { OReportRoutingModule } from '../components/report/o-report-routing.module';
 import { OReportHomeComponent } from '../components/report/o-report-home/o-report-home.component';
 import { OReportNewComponent } from '../components/report/o-report-new/o-report-new.component';
 import { OReportDetailComponent } from '../components/report/o-report-detail/o-report-detail.component';
@@ -31,20 +30,11 @@ export const OREPORT_DECLARATION_MODULES: any = [
 export const OREPORT_IMPORTS_MODULES: any = [
   CommonModule,
   OntimizeWebModule,
-  OReportRoutingModule,
   NgxExtendedPdfViewerModule,
   FlexLayoutModule,
   DragDropModule
 ]
-export const OREPORT_EXPORT_MODULES: any = [
-  OReportHomeComponent,
-  OReportNewComponent,
-  OReportDetailComponent,
-  OReportViewerComponent,
-  OReportRoutingModule,
-  ReportOnDemandComponent,
-  OReportsTranslatePipe
-];
+export const OREPORT_EXPORT_MODULES: any = [];
 export const OREPORT_ENTRY_COMPONENTS_MODULES: any = [
   OReportViewerComponent,
   ReportOnDemandComponent,
