3b10f042cfdae43d4a904ceaa6aaff4eddb67fa9
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2022-06-28 14:09:49 +0200

    Removed exports option in OReportModule (#69)

--- projects/ontimize-web-ngx-report/src/lib/ontimize-web-ngx-report.module.ts
+++ projects/ontimize-web-ngx-report/src/lib/ontimize-web-ngx-report.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { OREPORT_PROVIDERS } from './config/o-providers';
-import { OREPORT_DECLARATION_MODULES, OREPORT_ENTRY_COMPONENTS_MODULES, OREPORT_EXPORT_MODULES, OREPORT_IMPORTS_MODULES } from './config/o-components';
+import { OREPORT_DECLARATION_MODULES, OREPORT_ENTRY_COMPONENTS_MODULES, OREPORT_IMPORTS_MODULES } from './config/o-components';
 
 @NgModule({
   declarations: OREPORT_DECLARATION_MODULES,
   imports: OREPORT_IMPORTS_MODULES,
-  exports: OREPORT_EXPORT_MODULES,
   providers: OREPORT_PROVIDERS,
   entryComponents: OREPORT_ENTRY_COMPONENTS_MODULES
 })
