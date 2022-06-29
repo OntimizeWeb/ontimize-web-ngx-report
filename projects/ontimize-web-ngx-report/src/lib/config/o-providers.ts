8513439b33d1acb97c0f7a78576d90f984a60d73
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2022-06-29 14:43:47 +0200

    Updated o-providers

--- projects/ontimize-web-ngx-report/src/lib/config/o-providers.ts
+++ projects/ontimize-web-ngx-report/src/lib/config/o-providers.ts
@@ -1,18 +1,25 @@
 import { Injector } from "@angular/core";
 import { O_REPORT_SERVICE } from "ontimize-web-ngx";
 import { OAlertService } from "../services/o-alert.service";
-import { OFillReportService } from "../services/o-fill-report.service";
 import { OReportService } from "../services/o-report.service";
 import { OReportOnDemandService } from "../services/reports-on-demand.service";
 
 export function reportServiceFactory(injector: Injector): OReportService {
   return new OReportService(injector);
 }
+// export const OREPORT_PROVIDERS: any = [{ provide: O_REPORT_SERVICE, useFactory: reportOnDemandFactory, deps: [Injector, OReportService] },
+// {
+//   provide: OReportService,
+//   useFactory: reportServiceFactory,
+//   deps: [Injector]
+// },
+//   OAlertService
+// ];
 export const OREPORT_PROVIDERS: any = [{ provide: O_REPORT_SERVICE, useClass: OReportOnDemandService },
 {
   provide: 'report',
   useFactory: reportServiceFactory,
   deps: [Injector]
 },
-  OAlertService, OFillReportService
+  OAlertService
 ];
