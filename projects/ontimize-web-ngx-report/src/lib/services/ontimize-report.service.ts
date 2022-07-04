807bef4e627f535baf96e2f155e8b4308a542f46
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2022-07-04 08:36:02 +0200

    Removed method

--- projects/ontimize-web-ngx-report/src/lib/services/ontimize-report.service.ts
+++ projects/ontimize-web-ngx-report/src/lib/services/ontimize-report.service.ts
@@ -2,63 +2,23 @@ import { Injectable, Injector } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { DialogService, IReportService, OTableComponent } from 'ontimize-web-ngx';
 import { ReportOnDemandComponent } from '../components/report-on-demand/report-on-demand/report-on-demand.component';
-import { OReportViewerComponent } from '../components/report/o-report-viewer/o-report-viewer.component';
 import { Utils } from '../util/utils';
-import { OReportService } from './o-report.service';
-
 
 
 @Injectable({ providedIn: 'root' })
 export class OntimizeReportService implements IReportService {
   protected dialogService: DialogService;
   protected dialog: MatDialog;
-  protected reportService: OReportService;
 
   constructor(
     private injector: Injector
   ) {
-    this.dialogService = this.injector.get(DialogService);
-    this.dialog = this.injector.get(MatDialog);
-    this.reportService = this.injector.get(OReportService)
+    this.dialogService = this.injector.get<DialogService>(DialogService);
+    this.dialog = this.injector.get<MatDialog>(MatDialog);
   }
 
   openReportOnDemand(table: OTableComponent) {
     Utils.openModalVisor(this.dialog, ReportOnDemandComponent, table);
-  }
-
-  openFillReport(reportId: string, parametersValues: object, filter: object) {
-    this.reportService.configureService(this.reportService.getDefaultServiceConfiguration());
-    this.reportService.configureAdapter();
-    let kv = { 'UUID': reportId };
-    this.reportService.query(kv, null, 'getReport', {}).subscribe(
-      res => {
-        if (res && res.data.length && res.code === 0) {
-          let parameters = res.data[0].PARAMETERS;
-          let name = res.data[0].NAME;
-          let av = [];
-          if (parameters.length > 0) {
-            av = [reportId];
-            let values = Object.values(parametersValues);
-            for (let value of values) {
-              av.push(value);
-            }
-          } else {
-            av = [reportId];
-          }
-          const data = { 'params': av, 'filter': filter, 'name': name };
-          Utils.openModalVisor(this.dialog, OReportViewerComponent, data)
-
-        }
-      },
-      err => {
-        if (this.dialogService) {
-          this.dialogService.error('ERROR',
-            'SERVER_ERROR_MESSAGE');
-        }
-        console.log(err);
-      }
-    );
-
   }
 
 }
