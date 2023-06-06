3d15502ceae4bd0221321984391c507246118155
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2023-06-06 15:18:17 +0200

    Migration to Angular 12

--- projects/ontimize-web-ngx-report/src/lib/ontimize-web-ngx-report.module.ts
+++ projects/ontimize-web-ngx-report/src/lib/ontimize-web-ngx-report.module.ts
@@ -3,10 +3,9 @@ import { OREPORT_PROVIDERS } from './config/o-providers';
 import { OREPORT_DECLARATION_MODULES, OREPORT_ENTRY_COMPONENTS_MODULES, OREPORT_IMPORTS_MODULES } from './config/o-components';
 
 @NgModule({
-  declarations: OREPORT_DECLARATION_MODULES,
-  imports: OREPORT_IMPORTS_MODULES,
-  providers: OREPORT_PROVIDERS,
-  entryComponents: OREPORT_ENTRY_COMPONENTS_MODULES
+    declarations: OREPORT_DECLARATION_MODULES,
+    imports: OREPORT_IMPORTS_MODULES,
+    providers: OREPORT_PROVIDERS
 })
 
 export class OReportModule { }
