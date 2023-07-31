58964c9ad04464727bed3cb46b4af48ff7f6a650
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2023-07-31 08:04:03 +0200

    Updated karma.conf.js

--- projects/ontimize-web-ngx-report/karma.conf.js
+++ projects/ontimize-web-ngx-report/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ontimize-web-ngx'),
+      dir: require('path').join(__dirname, '../../coverage/ontimize-web-ngx-report'),
       subdir: '.',
       reporters: [
         { type: 'lcovonly' }
