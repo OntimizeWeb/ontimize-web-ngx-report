95306fa15f37e48215a38d3d7c96a91c795ed3d1
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2024-01-02 16:40:08 +0100

    New injection token `O_REPORT_DATA_SERVICE` (#174)
    * New injection token `O_REPORT_DATA_SERVICE`
    
    * Updated to correct literal
    
    * Updated peerDepedencies with "ontimize-web-ngx": "^8.14.6"

--- projects/ontimize-web-ngx-report/src/lib/config/o-providers.ts
+++ projects/ontimize-web-ngx-report/src/lib/config/o-providers.ts
@@ -1,18 +1,39 @@
-import { Injector } from "@angular/core";
-import { O_REPORT_SERVICE } from "ontimize-web-ngx";
-import { OAlertService } from "../services/o-alert.service";
-import { OReportStoreService } from "../services/o-report-store.service";
-import { OReportService } from "../services/o-report.service";
-import { OntimizeReportService } from "../services/ontimize-report.service";
+import { Injector } from '@angular/core';
+import { _getInjectionTokenValue, O_REPORT_SERVICE, Util } from 'ontimize-web-ngx';
+
+import { IReportDataProvider } from '../interfaces/report-data-provider.interface';
+import { OAlertService } from '../services/o-alert.service';
+import { OReportStoreService } from '../services/o-report-store.service';
+import { OReportService } from '../services/o-report.service';
+import { O_REPORT_DATA_SERVICE } from '../services/ontimize-report-data-base-provider.service';
+import { OntimizeReportDataProvider } from '../services/ontimize-report-data-provider.service';
+import { OntimizeReportService } from '../services/ontimize-report.service';
+
 
 export function reportServiceFactory(injector: Injector): OReportService {
   return new OReportService(injector);
 }
 
+
+export function reportDataFactory(injector: Injector): IReportDataProvider {
+  const provider = _getInjectionTokenValue(O_REPORT_DATA_SERVICE, injector);
+  const service = Util.createServiceInstance(provider, injector);
+  if (Util.isDefined(service)) {
+    return service;
+  } else {
+    return new OntimizeReportDataProvider(injector);
+  }
+
+}
 export const OREPORT_PROVIDERS: any = [{ provide: O_REPORT_SERVICE, useClass: OntimizeReportService },
 {
   provide: 'report',
   useFactory: reportServiceFactory,
+  deps: [Injector]
+},
+{
+  provide: OntimizeReportDataProvider,
+  useFactory: reportDataFactory,
   deps: [Injector]
 },
   OAlertService, OReportStoreService
