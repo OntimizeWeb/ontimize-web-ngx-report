b62a6ef1ed9aa5a6e7fe007f04b54fa3274fea42
tomfacal <88186642+tomfacal@users.noreply.github.com>
2024-01-24 13:16:12 +0100

    [8.3.1] Added skeleton to report viewer (#187)
    * added blank pdf
    
    * added skeleton

--- projects/ontimize-web-ngx-report/src/lib/config/o-components.ts
+++ projects/ontimize-web-ngx-report/src/lib/config/o-components.ts
@@ -13,6 +13,8 @@ import { SelectFunctionDialogComponent } from '../components/report-on-demand/se
 import { SavePreferencesDialogComponent } from '../components/report-on-demand/save-preferences-dialog/save-preferences-dialog.component';
 import { ApplyConfigurationDialogComponent } from '../components/report-on-demand/apply-configuration/apply-configuration-dialog.component';
 import { OReportsTranslatePipe } from '../util/o-reports-translate.pipe';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { OReportSkeletonComponent } from '../components';
 
 
 export const OREPORT_DECLARATION_MODULES: any = [
@@ -20,6 +22,7 @@ export const OREPORT_DECLARATION_MODULES: any = [
   OReportNewComponent,
   OReportDetailComponent,
   OReportViewerComponent,
+  OReportSkeletonComponent,
   ReportOnDemandComponent,
   StyleDialogComponent,
   SelectFunctionDialogComponent,
@@ -31,6 +34,7 @@ export const OREPORT_IMPORTS_MODULES: any = [
   CommonModule,
   OntimizeWebModule,
   NgxExtendedPdfViewerModule,
+  NgxSkeletonLoaderModule.forRoot(),
   FlexLayoutModule,
   DragDropModule
 ]
