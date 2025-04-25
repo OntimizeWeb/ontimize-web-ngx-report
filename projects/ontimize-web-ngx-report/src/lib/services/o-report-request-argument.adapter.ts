8e85a4eede68cd85039b1a804ac2ecef4882c684
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2025-04-25 07:46:47 +0200

    Renamed BaseQueryArgument with BaseRequestArgument

--- projects/ontimize-web-ngx-report/src/lib/services/o-report-request-argument.adapter.ts
+++ projects/ontimize-web-ngx-report/src/lib/services/o-report-request-argument.adapter.ts
@@ -4,7 +4,7 @@ import { OReportMappingUtils } from '../util/report-mapping-utils';
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class OReportQueryArgumentsAdapter extends OntimizeQueryArgumentsAdapter {
+export class OReportRequestArgumentsAdapter extends OntimizeQueryArgumentsAdapter {
 
   parseQueryParameters(args: any): any[] {
     args.columns = OReportMappingUtils.ontimizeMappingKeys(args.columns);
